fff90f9de50186baa3514b2a8e6c3cd3f7872532
Kapalik Khanal <kapalikkhanal@gmail.com>
2024-07-31 14:00:26 +0545

    Added tips

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import '@/styles/refresh.css'
 import '@/styles/button.css'
 import '@/styles/eyeButton.css'
 import '@/styles/scrollBar.css'
+import '@/styles/tipToolbar.css'
+import '@/styles/dropdown.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
